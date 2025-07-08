b44118223ba9799dbddd2c128a9ba0dc9ff21e7e
NINE-Lua <Trallen10@yahoo.com>
2025-07-07 20:54:28 -0500

    Update Guard.Aimbot.v1.1.lua

--- Guard.Aimbot.v1.1.lua
+++ Guard.Aimbot.v1.1.lua
@@ -42,7 +42,12 @@ local function getFacingTarget()
 		local model = result.Instance:FindFirstAncestorOfClass("Model")
 		if model then
 			local player = Players:GetPlayerFromCharacter(model)
-			if player and player.Team and player.Team.Name == "Guard" or player.Team.Name == "Swat" then
+			if player and player.Team and player.Team.Name == "Guard" then
+				local hrp = model:FindFirstChild(TARGET_PART)
+				if hrp then
+					return hrp
+				end
+			elseif if player and player.Team and player.Team.Name == "Swat" then
 				local hrp = model:FindFirstChild(TARGET_PART)
 				if hrp then
 					return hrp
